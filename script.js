8df201949fc693569cb8d1c4c3c097a128cd4385
cortinabryan <64225493+cortinabryan@users.noreply.github.com>
2022-03-12 14:55:52 -0800

    .some .every .flat .flatMap and sorting arrays

--- script.js
+++ script.js
@@ -61,10 +61,12 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHtml = '';
 
-  movements.forEach(function (mov, i) {
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const html = `<div class="movements__row">
@@ -134,7 +136,7 @@ const updateUI = function (acc) {
   calcDisplaySummary(currentAccount);
 };
 
-// Event Handler
+// Event Handlersssssss
 
 let currentAccount;
 
@@ -192,8 +194,58 @@ btnTransfer.addEventListener('click', function (e) {
     // Update UI
     updateUI(currentAccount);
   }
+  inputLoanAmount.value = '';
 });
 
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    // Add movement
+
+    currentAccount.movements.push(amount);
+
+    // Updated UI
+    updateUI(currentAccount);
+  }
+});
+
+// the find Index method
+
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+
+    console.log(index);
+    // .indexOf(23);
+
+    // Delete Account
+    accounts.splice(index, 1);
+
+    // Hide UI
+    containerApp.style.opacity = 0;
+  }
+  inputCloseUsername.value = inputClosePin.value = '';
+});
+
+// Sort Button
+
+let sorted = false;
+
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
+});
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -505,3 +557,97 @@ console.log(account);
 // Implementing Login
 
 // implementing transferss
+
+// Some and Every
+
+// console.log('Some and Every');
+
+// console.log(movements);
+
+// // Equality
+// console.log(movements.includes(-130));
+
+// // .some() - Condition
+// console.log(movements.some(mov => mov === -130));
+
+// const anyDeposits = movements.some(mov => mov > 0);
+
+// console.log(anyDeposits);
+
+// // .every() - condition
+
+// console.log(movements.every(mov => mov > 0));
+// console.log(account4.movements.every(mov => mov > 0));
+
+// // Separate callback
+
+// const deposit = mov => mov < 0;
+// // console.log(movements.some(deposit));
+// // console.log(movements.every(deposit));
+// // console.log(movements.filter(deposit));
+
+// .flat()
+
+// const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+
+// console.log(arr.flat());
+
+// const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+
+// console.log(arrDeep.flat(2));
+
+// const accountMovements = accounts.map(acc => acc.movements);
+
+// console.log(accountMovements);
+
+// const allMovements = accountMovements.flat();
+
+// console.log(allMovements);
+
+// const overallBalance = allMovements.reduce((acc, mov) => acc + mov, 0); // Adds the elements together
+
+// console.log(overallBalance);
+
+// // Chaining methods
+
+// const overallBalance2 = accounts
+//   .map(acc => acc.movements)
+//   .flat()
+//   .reduce((acc, mov) => acc + mov, 0);
+
+// console.log(overallBalance2);
+
+// // .flatMap()
+
+// const overallBalance3 = accounts
+//   .flatMap(acc => acc.movements)
+//   .reduce((acc, mov) => acc + mov, 0);
+
+// console.log(overallBalance3);
+
+// Sorting Arrays
+
+// Strings
+const owners = ['Bryan', 'Zach', 'Adam', 'Martha'];
+
+console.log(owners);
+console.log(owners.sort());
+console.log(owners);
+
+// Numbers
+
+console.log(movements);
+// console.log(movements.sort());
+
+// return < 0, A, B (keep order)
+// return > 0, B, A (switch order)
+
+// Ascendingas
+movements.sort((a, b) => a - b);
+
+console.log(movements);
+
+// Descending
+movements.sort((a, b) => b - a);
+
+console.log(movements);
